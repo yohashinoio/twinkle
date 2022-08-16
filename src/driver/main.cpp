8acc9b9f1cc4cfbcbbc402d54e98ec2515c135c2
yohashinoio <yohashinoio@gmail.com>
2022-08-16 20:55:09 +0900

    Refactor

--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -10,6 +10,22 @@
 #include <cstdlib>
 #include <iostream>
 
+// If the 'system' function could not be run, return std::nullopt; otherwise,
+// return linker exit status
+[[nodiscard]] std::optional<int>
+callLinker(const std::vector<std::filesystem::path>& files)
+{
+  if (!system(nullptr))
+    return std::nullopt;
+
+  std::string command = "cc";
+
+  for (const auto& r : files)
+    command += (' ' + r.string());
+
+  return system(command.c_str());
+}
+
 int main(const int argc, const char* const* const argv)
 {
   const auto context = spica::parseCmdlineOption(argc, argv);
@@ -26,11 +42,17 @@ int main(const int argc, const char* const* const argv)
       && std::holds_alternative<spica::AOTResult>(*result)) {
     const auto& aotresult = std::get<spica::AOTResult>(*result);
 
-    // Call linker
-    std::string command = "cc";
-    for (const auto& r : aotresult.created_files)
-      command += (' ' + r.string());
-    system(command.c_str());
+    {
+      // Call linker
+      const auto linker_exit_status = callLinker(aotresult.created_files);
+
+      if (linker_exit_status)
+        return *linker_exit_status;
+      else {
+        std::cerr << "Could not run linker!" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
   }
 
   return EXIT_SUCCESS;
